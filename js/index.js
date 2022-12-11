aae82753a65744cff3505259b07814fcd4266601
Sebastian Godoy <jugodoyc@unal.edu.co>
2022-12-10 22:55:25 -0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const documentReady = () => {
+  const heroTitle = document.querySelector('.hero__title');
+  const heroButtonSi = document.getElementById('heroButtonSi');
+  const heroButtonNo = document.querySelector('#heroButtonNo');
+
+  const nowZomosNobios = () => {
+    alert('💞 Now zomos ParTnEr 💞');
+    alert('😱Nuestra VoDa Is MañaNa😱');
+    location.href = 'https://www.youtube.com/shorts/A8_AuLDBmKc';
+  };
+
+  const evitarKCRompanMyBobo = () => {
+    heroButtonNo.style.position = 'absolute';
+    heroButtonNo.style.top = (Math.random() * window.innerHeight) + 'px';
+    heroButtonNo.style.left = (Math.random() * window.innerWidth) + 'px';
+  };
+
+  const partner = prompt('🥰 aaaa 🥰');
+  heroTitle.innerHTML += partner + ' ❤';
+
+  heroButtonSi.addEventListener('click', nowZomosNobios);
+  heroButtonNo.addEventListener('mouseover', evitarKCRompanMyBobo);
+};
+
+document.addEventListener('DOMContentLoaded', documentReady);
